f4bec21766c5209cec37c63183d24e79fb28bf83
rgmotisariya <radhikamotisariya@gmail.com>
2025-01-08 08:57:30 +0530

    navbar style update

--- Frontend/src/componnets/Navbar/Navbar.jsx
+++ Frontend/src/componnets/Navbar/Navbar.jsx
@@ -16,7 +16,6 @@ const MenuLinks = [
 ];
 
 function Navbar({ setShowlogin, setshowmanu }) {
-  
   const {
     setCartItems,
     setSubtotal,
@@ -72,138 +71,126 @@ function Navbar({ setShowlogin, setshowmanu }) {
   };
 
   return (
-    <div className="  container relative z-40 bg-white px-1">
-      <div className="py-1">
-        <div className="container  items-center ">
-          <div className="flex items-center justify-between gap-4">
-            {/* logo*/}
-            <Link to="/">
-              <p className="text-primary font-bold text-2xl " href="#Home">
-                MealMover
-              </p>
-            </Link>
+    <div className="container relative z-40 bg-white px-1 py-1">
+      <div className="flex items-center justify-between gap-4">
+        {/* Logo */}
+        <Link to="/">
+          <p className="text-primary font-bold text-2xl">MealMover</p>
+        </Link>
 
-            {/* manu*/}
-            <div className="hidden lg:block">
-              <ul className="flex items-center gap-4 ">
-                {MenuLinks.map((data, id) => (
-                  <li key={id}>
-                    <Link to={data.Link}>
-                      <p className="inline-block px-4 font-semibold text-gray-500 duration-200 hover:text-black ">
-                        {data.name}
-                      </p>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            {/*  Menubar right section*/}
-            <div className="flex items-center justify-between  md:gap-2 ">
-              <div className="relative group  sm:block ">
-                <input
-                  type="text"
-                  placeholder="search"
-                  className="search-bar hidden"
-                />
-                <IoSearchSharp className="absolute text-xl duration-200 -translate-y-1/2 tet-gray-600 group-hover:text-primary dark:text-gray-400 top-1/2 right-3" />
-              </div>
-
-              <button className="relative p-3">
-                <Link to="/Cart">
-                  <IoMdCart className="text-xl text-gray-500 hover:text-primary" />
+        {/* Menu */}
+        <div className="hidden lg:block">
+          <ul className="flex items-center gap-4">
+            {MenuLinks.map((data, id) => (
+              <li key={id}>
+                <Link to={data.Link}>
+                  <p className="inline-block px-4 font-semibold text-gray-500 duration-200 hover:text-black">
+                    {data.name}
+                  </p>
                 </Link>
-                {cartLength === 0 ? null : (
-                  <div className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-xs text-white bg-red-500 rounded-full ">
-                    {cartLength}
-                  </div>
-                )}
-              </button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-              {!userLoggedIn ? (
-                <button
-                  onClick={() => setShowlogin(true)}
-                  className="hover:text-white hover:bg-primary bg-gray-100 text-gray-700 rounded-full px-4 py-2 font-semibold transition duration-300 ease-in-out shadow-lg"
+        {/* Right Section */}
+        <div className="flex items-center gap-2">
+          {/* Search */}
+          <div className="relative group">
+            <input
+              type="text"
+              placeholder="search"
+              className="search-bar hidden"
+            />
+            <IoSearchSharp className="absolute text-xl duration-200 -translate-y-1/2 text-gray-600 group-hover:text-primary dark:text-gray-400 top-1/2 right-3" />
+          </div>
+
+          {/* Cart */}
+          <button className="relative p-3">
+            <Link to="/Cart">
+              <IoMdCart className="text-xl text-gray-500 hover:text-primary" />
+            </Link>
+            {cartLength !== 0 && (
+              <div className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-xs text-white bg-red-500 rounded-full">
+                {cartLength}
+              </div>
+            )}
+          </button>
+
+          {/* Authentication/Dropdown */}
+          {!userLoggedIn ? (
+            <button
+              onClick={() => setShowlogin(true)}
+              className="hover:text-white hover:bg-primary bg-gray-100 text-gray-700 rounded-full px-4 py-1 font-semibold transition duration-300 ease-in-out shadow-lg"
+            >
+              Sign in
+            </button>
+          ) : (
+            <div className="relative">
+              <img
+                src={assets.profile_icon}
+                alt="Profile"
+                className="w-6 h-6 rounded-full object-cover border-2 border-primary cursor-pointer"
+                onMouseEnter={() => setDropdownOpen(true)}
+                onMouseLeave={() => setDropdownOpen(false)}
+              />
+              {dropdownOpen && (
+                <div
+                  className="absolute right-0 mt-8 w-32 bg-white border rounded-lg shadow-lg z-50"
+                  onMouseEnter={() => setDropdownOpen(true)}
+                  onMouseLeave={() => setDropdownOpen(false)}
                 >
-                  Sign in
-                </button>
-              ) : (
-                <div className="relative flex items-center">
-                  {/* Profile Icon */}
-                  <img
-                    src={assets.profile_icon}
-                    alt="Profile"
-                    className="w-6 h-6 rounded-full object-cover border-2 border-primary cursor-pointer"
-                    onMouseEnter={() => setDropdownOpen(true)} // Open dropdown on hover
-                    onMouseLeave={() => setDropdownOpen(false)} // Close dropdown on mouse leave
-                  />
-
-                  {/* Dropdown Menu */}
-                  {dropdownOpen && (
-                    <div
-                      className="absolute right-0 mt-28 w-32 bg-white border rounded-lg shadow-lg z-50"
-                      onMouseEnter={() => setDropdownOpen(true)} // Keep dropdown open when hovered
-                      onMouseLeave={() => setDropdownOpen(false)} // Close dropdown on mouse leave
+                  <ul className="py-1 text-gray-700">
+                    <li
+                      className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
+                      onClick={() => {
+                        navigate("/profile");
+                        setDropdownOpen(false);
+                      }}
                     >
-                      <ul className="py-1 text-gray-700">
-                        <li
-                          className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
-                          onClick={() => {
-                            console.log("Profile clicked");
-                            navigate("/profile");
-                            setDropdownOpen(false);
-                          }}
-                        >
-                          <img
-                            src={assets.profile_icon}
-                            alt="Profile"
-                            className="w-4 h-4 inline-block mr-2"
-                          />
-                          Profile
-                        </li>
-                        <li
-                          className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
-                          onClick={() => {
-                            // Handle Order logic here
-                            console.log("Order clicked");
-                            navigate("/myOrder");
-                            setDropdownOpen(false);
-                          }}
-                        >
-                          <img
-                            src={assets.bag_icon}
-                            alt="Order"
-                            className="w-4 h-4 inline-block mr-2"
-                          />
-                          Order
-                        </li>
-                        <li
-                          className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
-                          onClick={() => {
-                            // Handle Logout logic here
-                            handleLogout();
-                            setDropdownOpen(false);
-                          }}
-                        >
-                          <img
-                            src={assets.logout_icon}
-                            alt="Logout"
-                            className="w-4 h-4 inline-block mr-2"
-                          />
-                          Logout
-                        </li>
-                      </ul>
-                    </div>
-                  )}
+                      <img
+                        src={assets.profile_icon}
+                        alt="Profile"
+                        className="w-4 h-4 inline-block mr-2"
+                      />
+                      Profile
+                    </li>
+                    <li
+                      className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
+                      onClick={() => {
+                        navigate("/myOrder");
+                        setDropdownOpen(false);
+                      }}
+                    >
+                      <img
+                        src={assets.bag_icon}
+                        alt="Order"
+                        className="w-4 h-4 inline-block mr-2"
+                      />
+                      Order
+                    </li>
+                    <li
+                      className="px-4 py-2 cursor-pointer hover:bg-gray-100 transition"
+                      onClick={handleLogout}
+                    >
+                      <img
+                        src={assets.logout_icon}
+                        alt="Logout"
+                        className="w-4 h-4 inline-block mr-2"
+                      />
+                      Logout
+                    </li>
+                  </ul>
                 </div>
               )}
-
-              <IoMenu
-                onClick={() => setshowmanu(true)}
-                className="lg:hidden text-2xl text-gray-500 hover:text-primary"
-              />
             </div>
-          </div>
+          )}
+
+          {/* Mobile Menu */}
+          <IoMenu
+            onClick={() => setshowmanu(true)}
+            className="lg:hidden text-2xl text-gray-500 hover:text-primary"
+          />
         </div>
       </div>
     </div>
